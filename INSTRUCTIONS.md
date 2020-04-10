e62964e59df03adb01fcae683f439b6ab66bc05a
teosnare <m.murtas.radionav@gmail.com>
2020-04-10 09:00:27 +0200

    added docker-compose.yml

---
+++ INSTRUCTIONS.md
@@ -0,0 +1,10 @@
+# Executing with docker-compose
+
+To launch the tensorflow docker image with docker-compose, you will need to specify the option
+runtime: nvidia
+
+To make this work, you need a docker-compose version >= 1.19. Please visit
+
+https://docs.docker.com/compose/install/#install-compose
+
+to install the latest version of docker-compose.
--- serve-cracks.sh
+++ serve-cracks.sh
@@ -2,5 +2,5 @@
 
 export SERVING_MODEL=simplenet_cracks8020
 #docker run -t --rm --runtime=nvidia -p 8501:8501 -v $(realpath $PWD/models):/models/ --name crack_classification_service -e MODEL_NAME=$SERVING_MODEL tensorflow/serving:1.12.0-gpu
-docker run -t --rm --runtime=nvidia -p 8501:8501 -v $(realpath $PWD/models):/models/ --name crack_classification_service -e MODEL_NAME=$SERVING_MODEL tensorflow/serving:latest
+docker run -t --rm --runtime=nvidia -p 8501:8501 -v $(realpath $PWD/models):/models/ --name crack_classification_service -e MODEL_NAME=$SERVING_MODEL tensorflow/serving:latest-gpu
 
